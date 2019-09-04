80543678e9ef4c58bdd2ae23085bbf915f3a229d
Alex Telon <alex.telon@cybercom.com>
2019-09-04 16:09:06 +0200

    Down/up animates line by line
    Added the ability to show the slide line by line with up and down
    arrows.
    
    Also made sure that going up/down past the end/beginning of the slide
    goes to the next one.

--- calc.py
+++ calc.py
@@ -19,6 +19,25 @@ def get_slides(file):
 
     return slides
 
+def print_slide(stdscr, slide, lines_to_print=-1)
+    for i, line in enumerate(slide.splitlines()):
+        format = curses.A_NORMAL
+        if line.startswith('# '):
+            line = line[2::]
+            format = curses.A_STANDOUT
+            # curses.A_BOLD is another option
+
+        stdscr.addstr(i, 0, line, format)
+
+        if i == lines_to_print:
+            break
+
+    # debug
+    # stdscr.addstr(15, 0, "_:" + str(lines_to_print))
+    stdscr.refresh()
+
+
+
 def main(stdscr):
     stdscr.clear()
     # Disable cursor highlight
@@ -27,32 +46,48 @@ def main(stdscr):
     slides = get_slides('presentation.md')
 
     slide_nr = 1
+    animate = -1
     while True:
         slide = slides[slide_nr % len(slides)]
 
-        for i, line in enumerate(slide.splitlines()):
-            format = curses.A_NORMAL
-            if line.startswith('# '):
-                line = line[2::]
-                format = curses.A_STANDOUT
-                # curses.A_BOLD is another option
-            stdscr.addstr(i, 0, line, format)
+        # if we were to animate the parts of the slide line by line, how many steps would we have
+        slide_animation_length = len(slide.splitlines())
 
-        stdscr.refresh()
+        print_slide(stdscr, slide, animate)
 
         char = stdscr.getch()
         if char == 113: break  # qk
-        elif char == curses.KEY_RIGHT or char == curses.KEY_DOWN:
+        elif char == curses.KEY_RIGHT:
             slide_nr += 1
-        elif char == curses.KEY_LEFT or char == curses.KEY_UP:
+            # reset animation so we show the full slide
+            animate = -1
+
+        elif char == curses.KEY_LEFT:
             slide_nr -= 1
+            # reset animation so we show the full slide
+            animate = -1
+
+        elif char == curses.KEY_DOWN:
+            animate += 1
+
+            # If we go past the end of the slide go the beginning of the next one without showing contents
+            if animate >= slide_animation_length:
+                animate = 1
+                slide_nr += 1
+
+        elif char == curses.KEY_UP:
+            animate -= 1
+
+            # If we go past 0 then we want to the previous slide and show the whole thing
+            if animate < 0:
+                animate = -1
+                slide_nr -= 1
         else:
             slide_nr += 1
 
         # Dont wrap around backwards
         if slide_nr < 0:
             slide_nr = 0
-
         if slide_nr >= len(slides):
             slide_nr = len(slides)
 
