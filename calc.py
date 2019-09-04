fbcf97e0e508ff7fdec62f4092584c11d71b98dc
Alex Telon <alex.telon@cybercom.com>
2019-09-04 14:02:01 +0200

    Curses sample
    Took a sample from curses and modified it a bit so it displays a new
    line every time a char is pressed.
    
    The goal is to read in a file in markdown, show a slide at a time using
    this library.

--- calc.py
+++ calc.py
@@ -1,1 +1,18 @@
-print('hi')
+from curses import wrapper
+
+def main(stdscr):
+    WIDTH = 100
+
+    # Clear screen
+    stdscr.clear()
+
+    for i in range(0, 11):
+        v = i-10
+        stdscr.addstr(i, int(WIDTH/2), '10 divided by {} is {}'.format(v, 2))
+        stdscr.refresh()
+        stdscr.getkey()
+
+        # Clear screen
+        stdscr.clear()
+
+wrapper(main)
